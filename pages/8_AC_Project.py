44dba45082bdcf36ab4b42cbf49093704405f0d7
Lei I. Parato <95401339+lei-karma002@users.noreply.github.com>
2024-05-14 09:57:08 +0000

    _

--- pages/8_AC_Project.py
+++ pages/8_AC_Project.py
@@ -200,34 +200,34 @@ def main():
             st.write("Decrypted Data (XOR Cipher):", decrypted_data.decode())
             st.success("Data encrypted and decrypted with XOR Cipher successfully!")
 
-    st.header("Diffie-Hellman")
-    p = st.number_input("Enter prime number:", min_value=2, step=1, format="%d")
-    g = st.number_input(f"Enter the primitive root of {p}:", min_value=2, step=1, format="%d")
-    private_key = st.number_input("Enter your private key:", min_value=1, step=1, format="%d")
-    received_public_key = st.number_input("Enter received public key:", min_value=1, step=1, format="%d")
+        st.header("Diffie-Hellman")
+        p = st.number_input("Enter prime number:", min_value=2, step=1, format="%d")
+        g = st.number_input(f"Enter the primitive root of {p}:", min_value=2, step=1, format="%d")
+        private_key = st.number_input("Enter your private key:", min_value=1, step=1, format="%d")
+        received_public_key = st.number_input("Enter received public key:", min_value=1, step=1, format="%d")
 
-    if st.button("Generate Public Key"):
-        if not prime_checker(p):
-            st.error("Number is not prime, please enter a prime number.")
-        elif not primitive_check(g, p):
-            st.error(f"Number is not a primitive root of {p}, please try again!")
-        elif private_key >= p:
-            st.error(f"Private key should be less than {p}, please enter again!")
-        else:
-            public_key, shared_secret = diffie_hellman(p, g, private_key, received_public_key)
-            st.success(f"Your public key: {public_key}")
+        if st.button("Generate Public Key"):
+            if not prime_checker(p):
+                st.error("Number is not prime, please enter a prime number.")
+            elif not primitive_check(g, p):
+                st.error(f"Number is not a primitive root of {p}, please try again!")
+            elif private_key >= p:
+                st.error(f"Private key should be less than {p}, please enter again!")
+            else:
+                public_key, shared_secret = diffie_hellman(p, g, private_key, received_public_key)
+                st.success(f"Your public key: {public_key}")
 
-    message = st.text_input("Type your message:")
-    if st.button("Send"):
-        key = shared_secret % 95
-        encrypted_message = caesar_encrypt(message, key)
-        st.success(f"You Sent: {encrypted_message}")
+        message = st.text_input("Type your message:")
+        if st.button("Send"):
+            key = shared_secret % 95
+            encrypted_message = caesar_encrypt(message, key)
+            st.success(f"You Sent: {encrypted_message}")
 
-    received_message = st.text_input("Enter received message:")
-    if st.button("Receive"):
-        key = shared_secret % 95
-        decrypted_message = caesar_decrypt(received_message, key)
-        st.success(f"Received message: {decrypted_message}")
+        received_message = st.text_input("Enter received message:")
+        if st.button("Receive"):
+            key = shared_secret % 95
+            decrypted_message = caesar_decrypt(received_message, key)
+            st.success(f"Received message: {decrypted_message}")
 
 
     st.markdown("![Alt Text](https://tenor.com/view/shaq-shimmy-gif-6579961127426913411.gif)")
